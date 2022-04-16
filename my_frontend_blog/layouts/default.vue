3b1f70922f49a350b5fa52b8ecf34dcf02ea2197
greatlaki <greatlaki911@gmail.com>
2022-04-16 18:23:36 +0300

    Added components: Slider, Footer

--- my_frontend_blog/layouts/default.vue
+++ my_frontend_blog/layouts/default.vue
@@ -8,10 +8,14 @@
 
 <script>
 import Navbar from "~/components/Navbar";
+import Slider from "~/components/Slider";
+import Footer from "@/components/Footer";
 
 export default {
   components: {
     Navbar,
+    Slider,
+    Footer,
   }
 };
 </script>
