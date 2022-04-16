0f674a9c39d4497f80cc9e73cb1574adc3795381
greatlaki <greatlaki911@gmail.com>
2022-04-16 19:38:02 +0300

    Components were added (Header, Aside, Comments)

---
+++ my_frontend_blog/components/Aside.vue
@@ -0,0 +1,65 @@
+<template>
+  <div className="col-md-4">
+    <div className="card my-4">
+      <h5 className="card-header">Теги</h5>
+      <div className="card-body">
+        <div className="d-flex flex-wrap">
+          <a href="" className="m-2 h5">#CSS</a>
+          <a href="" className="m-2 h5">#JavaScript</a>
+          <a href="" className="m-2 h5">#DRF</a>
+          <a href="" className="m-2 h5">#NuxtJS</a>
+          <a href="" className="m-2 h5">#Django</a>
+          <a href="" className="m-2 h5">#VueJS</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="card my-4">
+      <h5 className="card-header">Последние статьи</h5>
+      <div className="card-body">
+        <h5 className="card-title">Раз пост</h5>
+        <p className="card-text">Какой-то вступительный текст. Какой-то вступительный текст. Какой-то вступительный
+          текст.</p>
+        <a href="#" className="card-link">Ссылка на статью</a>
+      </div>
+      <hr>
+      <div className="card-body">
+        <h5 className="card-title">Два пост</h5>
+        <p className="card-text">Какой-то вступительный текст. Какой-то вступительный текст. Какой-то вступительный
+          текст.</p>
+        <a href="#" className="card-link">Ссылка на статью</a>
+      </div>
+      <hr>
+      <div className="card-body">
+        <h5 className="card-title">Три пост</h5>
+        <p className="card-text">Какой-то вступительный текст. Какой-то вступительный текст. Какой-то вступительный
+          текст.</p>
+        <a href="#" className="card-link">Ссылка на статью</a>
+      </div>
+      <hr>
+      <div className="card-body">
+        <h5 className="card-title">Четыре пост</h5>
+        <p className="card-text">Какой-то вступительный текст. Какой-то вступительный текст. Какой-то вступительный
+          текст.</p>
+        <a href="#" className="card-link">Ссылка на статью</a>
+      </div>
+      <hr>
+      <div className="card-body">
+        <h5 className="card-title">Пять пост</h5>
+        <p className="card-text">Какой-то вступительный текст. Какой-то вступительный текст. Какой-то вступительный
+          текст.</p>
+        <a href="#" className="card-link">Ссылка на статью</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "aside.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
