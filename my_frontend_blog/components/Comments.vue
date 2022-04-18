f62a511f70f46c5d808584bde5d014797f419f9e
greatlaki <greatlaki911@gmail.com>
2022-04-18 10:46:43 +0300

    Adding comments

--- my_frontend_blog/components/Comments.vue
+++ my_frontend_blog/components/Comments.vue
@@ -2,13 +2,16 @@
   <div>
     <div class="card my-4">
       <h5 class="card-header">Прокомментируй:</h5>
-      <div class="card-body">
-        <form>
+      <div class="card-body" v-if="loggedIn">
+        <form @submit.prevent="addComment">
           <div class="form-group">
-            <textarea class="form-control" rows="3"></textarea>
+            <textarea class="form-control" rows="3" v-model="new_comment"></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Отправить</button>
+          <button type="submit" class="btn btn-primary" :disabled='!isComplete'>Отправить</button>
         </form>
+      </div>
+      <div v-else>
+        <h6 class="card-header"><nuxt-link to="/signin">Авторизуйтесь</nuxt-link> или <nuxt-link to="/signup">зарегистрируйтесь</nuxt-link> чтобы оставить комментарий</h6>
       </div>
     </div>
     <div class="media mb-4" v-for="comment in comments" :key="comment.id">
@@ -22,8 +25,41 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  props: ['comments'],
+  props: ['comments', 'post'],
+  data() {
+    return {
+      new_comment: '',
+    }
+  },
+  methods: {
+    async addComment() {
+      try {
+        let response = await this.$axios.post('http://localhost:8000/api/comments/', {
+        post: this.$props.post.slug,
+        username: this.user.username,
+        text: this.new_comment,
+      })
+        this.new_comment = '';
+        this.comments.splice(0, 0, response.data)
+      console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+  computed: {
+    loggedIn() {
+      return this.$auth.loggedIn
+    },
+    user() {
+      return this.$auth.user
+    },
+    isComplete () {
+      return this.new_comment;
+    }
+  }
 }
 </script>
 
